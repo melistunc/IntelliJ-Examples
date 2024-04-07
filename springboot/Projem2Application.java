cb10da313add95c7a80744dde1b3407ba89b60a0
melis <131777538+melistunc@users.noreply.github.com>
2024-04-07 16:22:41 +0300

    Add files via upload

---
+++ springboot/Projem2Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projem2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projem2Application.
+                class, args);
+    }
+
+}
